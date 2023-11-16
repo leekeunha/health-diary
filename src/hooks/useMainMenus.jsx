67b5b370b7680a575cdfda1621aa7372206d5832
leekeunha <leetoya@naver.com>
2023-11-16 15:59:51 +0900

    adsf

--- src/hooks/useMainMenus.jsx
+++ src/hooks/useMainMenus.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { getMainMenus as fetchProducts } from '../api/firebase';
 
 export default function useMainMenus() {
